4a1d727cb8a57533793f81ced3bd494dfb67bca9
MYudina <MYudina@PC_MSK-20470>
2023-10-02 10:32:35 +0300

    исправление

---
+++ Homework_13/IEnumerableЕxtension.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Homework_13
+{
+    public static class IEnumerableЕxtension
+    {
+        public static IEnumerable<int> Top<T>(this IEnumerable<T> list, int p)
+        {
+            bool isPercentage = p < 100 && p > 0;
+
+            if (isPercentage)
+            {
+                var elementCount = Math.Ceiling(list.Count() * (double)p / 100);
+
+                return (IEnumerable<int>)list.OrderByDescending(x => x).Take(Convert.ToInt32(elementCount));
+            }
+            else
+            {
+                throw new ArgumentException("Заданное количество процентов должно быть больше 0 и меньше 100!");
+            }
+
+        }
+    }
+}
